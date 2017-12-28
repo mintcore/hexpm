11bdf8d0ba18cef63b00c6b404ea8adec058c451
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2017-12-28 18:55:39 +0100

    Add new organization form

--- lib/hexpm/web/controllers/test_controller.ex
+++ lib/hexpm/web/controllers/test_controller.ex
@@ -47,7 +47,7 @@ defmodule Hexpm.Web.TestController do
   end
 
   def repo(conn, %{"name" => name}) do
-    Repositories.create(name, conn.assigns.current_user)
+    Repositories.create(name, conn.assigns.current_user, audit: {%User{}, "TEST"})
     send_resp(conn, 204, "")
   end
 
